9d46e5f47b16690659c6d5a8e4125f7b5b89f56c
anuj123-45 <anuj1358.be20@chikara.edu.in>
2024-01-10 18:35:27 +0530

    todo html css done

---
+++ Task 2 (To Do List)/script.js
@@ -0,0 +1,12 @@
+function openModal() {
+    document.getElementById('addTaskModal').style.display = 'flex';
+    document.getElementById('edit').style.display = "none";
+    document.getElementById('add').style.display = "flex";
+    
+  }
+
+
+
+  function closeModal() {
+    document.getElementById('addTaskModal').style.display = 'none';
+  }
