f31546034f7a0d23488a849082f88ea66de74d53
anuj123-45 <anuj1358.be20@chikara.edu.in>
2024-02-19 19:04:16 +0530

    up to date

--- Task 2 (To Do List)/script.js
+++ Task 2 (To Do List)/script.js
@@ -1,7 +1,6 @@
 
 // function for open modal
 
-
 function openModal() {
   document.getElementById('addTaskModal').style.display = 'flex';
   document.getElementById('edit').style.display = "none";
@@ -14,7 +13,6 @@ function openModal() {
 function closeModal() {
   document.getElementById('addTaskModal').style.display = 'none';
 }
-
 
 
 // function for getting tasks
